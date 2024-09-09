9238ec3b33938830967713e4e819996efc3b60d7
pyrs <yagosousa2512@gmail.com>
2024-09-09 06:20:26 -0300

    Update: Ajustando as respostas ao front

--- routes/livros.js
+++ routes/livros.js
@@ -14,33 +14,35 @@ router.get('/', async (req, res) => {
         console.log(err)
     }
 
-    res.sendStatus(500)
+    return res.sendStatus(500)
 })
 
 router.get('/:id', async (req, res) => {
     try {
         const { id } = req.params
         const livro = await LivrosController.get(id)
-        if (livro == null) return res.status(404).send(null)
+        if (livro == null) return res.sendStatus(404)
 
         return res.send(livro)
     } catch (err) {
         console.log(err)
     }
 
-    res.sendStatus(500)
+    return res.sendStatus(500)
 })
 
 router.post('/', async (req, res) => {
     try {
         if (!validaLivro(req.body))
-            return res.status(400).send(null)
+            return res.status(400).send('Informações do livro invalidas')
 
         const { id } = req.body
         const livroConflict = await LivrosController.get(id)
-        if (livroConflict != null) return res.status(409).send(null)
+        if (livroConflict != null) return res.status(409).send('Livro ja existe')
 
-        return res.status(201).send(await LivrosController.create(req.body))
+        const result = await LivrosController.create(req.body);
+        if (result !== null) return res.status(201).send('Livro criado com sucesso')
+        else return res.status(400).send('O livro não pode ser criado')
     } catch (err) {
         console.log(err)
     }
@@ -52,19 +54,19 @@ router.put('/:id', async (req, res) => {
     try {
         const { id } = req.params
         const livro = await LivrosController.get(id)
-        if (livro == null) return res.status(404).send(null)
+        if (livro == null) return res.status(404).send('Livro não encontrado')
 
         if (!validaLivro(req.body))
-            return res.status(400).send(null)
+            return res.status(400).send('Informações do livro invalidas')
 
         if (id != req.body.id) {
             const livroConflict = await LivrosController.get(req.body.id)
-            if (livroConflict != null) return res.status(409).send(null)
+            if (livroConflict != null) return res.status(409).send('Livro ja existe')
         }
 
         const result = await LivrosController.update(id, req.body)
-        if (result >= 1) return res.send(livro)
-        else res.sendStatus(400)
+        if (result >= 1) return res.send({ 'mensagem': 'Livro atualizado com sucesso' })
+        else res.status(400).send('O livro não pode ser atualizado')
     } catch (err) {
         console.log(err)
     }
@@ -76,11 +78,11 @@ router.delete('/:id', async (req, res) => {
     try {
         const { id } = req.params
         const livro = await LivrosController.get(id)
-        if (livro == null) return res.status(404).send(null)
+        if (livro == null) return res.status(404).send({ 'mensagem': 'Livro não encontrado' })
 
         const result = await LivrosController.delete(id)
-        if (result >= 1) return res.send()
-        else res.sendStatus(400)
+        if (result >= 1) return res.send({ 'mensagem': 'Livro deletado com sucesso' })
+        else res.status(400).send({ 'mensagem': 'O livro não pode ser deletado' })
     } catch (err) {
         console.log(err)
     }
